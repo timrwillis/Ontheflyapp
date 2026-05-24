c9278b3a7ddc4ec0dda7c0cea3b7b1579f63b9e7
Tim Willis <timrwillis@gmail.com>
2026-05-24 13:37:18 -0500

    fix(onboarding): replace role emojis with line icons from @expo/vector-icons

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -58,4 +58,7 @@ export const Colors = {
     background: "#151718",
     tint: tintColorDark,
     icon: "#9BA1A6",
-    
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+  },
+};
